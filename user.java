4622dd2ca19e0ea3ef099c2292f59d9f157d8abf
Akshay Khajuriya <241728994+cx-akshay-khajuriya@users.noreply.github.com>
2026-09-09 12:06:35 +0530

    Create user.java

---
+++ user.java
@@ -0,0 +1,20 @@
+public class DemoLogin {
+
+    private static final String DB_PASSWORD = "admin123";
+    private static final String API_KEY = "demo-api-key-12345";
+
+    public static void main(String[] args) {
+        System.out.println("Connecting with password: " + DB_PASSWORD);
+    }
+
+   public static void findUser(Connection connection, String username) throws SQLException {
+        String query = "SELECT * FROM users WHERE username = '" + username + "'";
+
+        Statement statement = connection.createStatement();
+        ResultSet result = statement.executeQuery(query);
+
+        while (result.next()) {
+            System.out.println(result.getString("username"));
+        }
+    }
+}
